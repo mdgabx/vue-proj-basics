ffa6e54758c7d8e0e02b9014c07c6be0a07a77d5
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-31 20:20:35 +0800

    add: vue styles and scoped

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <h1>{{ title }}</h1>
+
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click</button>
+
+  <ModalTest :header="header" :arr="[1,2,3,4]" theme="sale"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ModalTest from './components/ModalTest.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ModalTest
+  },
+  data() {
+    return {
+      title: 'TEst tislt',
+      header: 'Test my header'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ font-family: Consolas;
+ margin: 0;
+ padding: 0;
 }
+
+
 </style>
---
+++ src/components/ModalTest.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="modalbg">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>{{ header }}</h1>
+            <p>modal content</p>
+            <ul>
+                <li v-for="i in arr" :key="i">
+                    <p>{{ i }}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['header', 'arr', 'theme']
+    }
+    
+</script>
+
+<style scoped>
+    .modalbg {
+        background: rgba(0,0,0, 0.2);
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        margin: auto;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    p {
+    color: orange
+    }
+
+    .sale {
+        background: red !important;
+    }
+
+    .modal {
+        padding: 40px;
+        background: white;
+        border-radius: 20px;
+        margin: auto;
+    }
+</style>
